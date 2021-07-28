d5092e9ced500378be63a34b3c17b4e2dfe2e973
Jiajun <2547153J@student.gla.ac.uk>
2021-07-28 21:47:33 +0800

    chp6 complete

--- rango/views.py
+++ rango/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from rango.models import Category, Page
 def index(request):
     category_list= Category.objects.order_by('-likes')[:5]
+    page_list= Page.objects.order_by('-views')[:5]
     context_dict={}
     context_dict['boldmessage']='Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories']=category_list
+    context_dict['pages']=page_list
     return render(request,'rango/index.html',context=context_dict)
     #return HttpResponse("Rango says hey there partner!(<a href='/rango/about/'>About</a>)")
 def about(request):
