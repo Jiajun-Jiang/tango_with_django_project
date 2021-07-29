875f05ce37458bc81f2b6135e76c41daf61f12a9
Jiajun <2547153J@student.gla.ac.uk>
2021-07-29 10:50:18 +0800

    chp7 complete

--- rango/views.py
+++ rango/views.py
@@ -1,45 +1,76 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
-from rango.form import CategoryForm
+from rango.forms import CategoryForm, PageForm
 from django.shortcuts import redirect
+from django.urls import reverse
+
+
 def index(request):
-    category_list= Category.objects.order_by('-likes')[:5]
-    page_list= Page.objects.order_by('-views')[:5]
-    context_dict={}
-    context_dict['boldmessage']='Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories']=category_list
-    context_dict['pages']=page_list
-    return render(request,'rango/index.html',context=context_dict)
-    #return HttpResponse("Rango says hey there partner!(<a href='/rango/about/'>About</a>)")
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    return render(request, 'rango/index.html', context=context_dict)
+    # return HttpResponse("Rango says hey there partner!(<a href='/rango/about/'>About</a>)")
+
+
 def about(request):
-    your_name={'your_name':'Jiajun'}
-    #return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a> .")
-    return render(request,'rango/about.html',your_name)
+    your_name = {'your_name': 'Jiajun'}
+    # return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a> .")
+    return render(request, 'rango/about.html', your_name)
+
 
 def show_category(request, category_name_slug):
-    context_dict={}
+    context_dict = {}
 
     try:
-        category= Category.objects.get(slug=category_name_slug)
-        pages=Page.objects.filter(category=category)
-        context_dict['pages']= pages
-        context_dict['category']=category
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
     except:
-        context_dict['pages']= None
-        context_dict['category']=None
-    return render(request,'rango/category.html', context=context_dict)
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    return render(request, 'rango/category.html', context=context_dict)
+
 
 def add_category(request):
-        form=CategoryForm()
+    form = CategoryForm()
 
-        if request.method=='POST':
-            form=CategoryForm(request.POST)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
 
-            if form.is_valid():
-                form.save(commit=True)
-                return redirect('/rango/')
-            
-            else:
-                print(form.errors)
-        return render(request,'rango/add_category.html',{'form':form})
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(requset,category_name_slug):
+    try:
+        category=Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category=None
+    if category is None:
+        return redirect('/rango/')
+    form=PageForm()
+    if requset.method=='POST':
+        form=PageForm(requset.POST)
+        if form.is_valid():
+            if category:
+                page=form.save(commit=False)
+                page.category=category
+                page.views=0
+                page.save()
+
+                return redirect(reverse('rango:show_category',kwargs={'category_name_slug':category_name_slug}))
+        else:
+            print(form.errors)
+    context_dict={'form':form,'category':category}
+    return render(requset,'rango/add_page.html',context=context_dict)
